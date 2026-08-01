da33eab0c1f5cdf22fa4024ca69fa5cc04ef9fb9
codecraft <codecraftwt@gmail.com>
2026-08-01 12:09:40 +0530

    CLOUDINARY applied

--- config/plugins.js
+++ config/plugins.js
@@ -16,4 +16,25 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {
+          upload_preset: env('CLOUDINARY_UPLOAD_PRESET', 'KIT_strapi_media'),
+          folder: env('CLOUDINARY_FOLDER', 'KIT-Website'),
+        },
+        uploadStream: {
+          upload_preset: env('CLOUDINARY_UPLOAD_PRESET', 'KIT_strapi_media'),
+          folder: env('CLOUDINARY_FOLDER', 'KIT-Website'),
+        },
+        delete: {},
+      },
+    },
+  },
 });
